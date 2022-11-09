a98dc4908a7cf1f3c5329ae6447858359dbc2675
marlabarrr <117593547+marlabarrr@users.noreply.github.com>
2022-11-09 11:50:17 -0300

    Carla Barrera TPN1

---
+++ _projects/sketch.js
@@ -0,0 +1,300 @@
+//Título: "Qr for me"
+let font, // variable para texto
+  fontsize = 40; //tamaño de fuente
+var angulo = 0; //variable para la rotación de las figuras
+var entro = true;
+function setup() {
+  let canvas = createCanvas(512, 512);
+  canvas.parent('div-sketch');
+  background(200);
+  
+  //TEXTO DE INICIO
+  textFont();
+  textSize(fontsize);
+  textAlign(CENTER, CENTER);
+  text("Bienvenidx", width / 2, 240);
+
+  textSize(fontsize / 2);
+  text('Presione "Enter" para simular el Qr', width / 2, 280);
+  text("que le gustaría ser", width / 2, 300);
+  
+}
+
+function draw() {
+  
+  if (keyCode === ENTER) {
+    //cuadrados negros y blancos de los extremos
+    //cuadrado pequeño superior izquierdo
+    if(entro){
+      background(200);
+      entro = false; //booleana 
+    }
+    push();
+    noStroke();
+    fill(255);
+    rect(55, 54, 40, 40);
+
+    fill(0);
+    rect(55, 54, 30, 30);
+
+    fill(255);
+    rect(55, 54, 20, 20);
+
+    fill(0);
+    rect(55, 54, 10, 10);
+
+    pop();
+
+    //cuadrado pequeño superior derecho
+    push();
+    noStroke();
+    fill(255);
+    rect(455, 54, 40, 40);
+
+    fill(0);
+    rect(455, 54, 30, 30);
+
+    fill(255);
+    rect(455, 54, 20, 20);
+
+    fill(0);
+
+    rect(455, 54, 10, 10);
+    pop();
+    //cuadro pequeño inferior izquierdo
+
+    noStroke();
+    fill(255);
+    rect(55, 454, 40, 40);
+
+    fill(0);
+    rect(55, 454, 30, 30);
+
+    fill(255);
+    rect(55, 454, 20, 20);
+
+    fill(0);
+    rectMode(RADIUS);
+    rect(55, 454, 10, 10);
+
+    //cuadrado pequeño inferior derecho
+    noStroke();
+    fill(255);
+    rect(455, 454, 40, 40);
+
+    noStroke();
+    fill(0);
+    rect(455, 454, 30, 30);
+
+    noStroke();
+    fill(255);
+    rect(455, 454, 20, 20);
+
+    noStroke();
+    fill(0);
+    rect(455, 454, 10, 10);
+
+    //cuadrado grande superior izquierdo
+    push();
+    noStroke();
+    fill(255);
+    rectMode(RADIUS);
+    rect(116, 114, 60, 60);
+
+    fill(0);
+    rect(116, 114, 50, 50);
+
+    fill(255);
+    rect(116, 114, 40, 40);
+
+    fill(0);
+    rect(116, 114, 30, 30);
+
+    fill(255);
+    rect(116, 114, 20, 20);
+
+    noStroke();
+    fill(0);
+    rect(116, 114, 10, 10);
+
+    //cuadrado grande superior derecho
+    noStroke();
+    fill(255);
+    rectMode(RADIUS);
+    rect(394, 115, 60, 60);
+
+    fill(0);
+    rect(394, 115, 50, 50);
+
+    fill(255);
+    rect(394, 115, 40, 40);
+
+    fill(0);
+    rect(394, 115, 30, 30);
+
+    fill(255);
+    rect(394, 115, 20, 20);
+
+    fill(0);
+    rect(394, 115, 10, 10);
+
+    //cuadrado grande inferior izquierdo
+    noStroke();
+    fill(255);
+    rect(115, 395, 60, 60);
+
+    fill(0);
+    rect(115, 395, 50, 50);
+
+    fill(255);
+    rect(115, 395, 40, 40);
+
+    fill(0);
+    rect(115, 395, 30, 30);
+
+    fill(255);
+    rect(115, 395, 20, 20);
+
+    fill(0);
+    rect(115, 395, 10, 10);
+    //cuadrado grande inferior derecho
+    noStroke();
+    fill(255);
+    rect(395, 397, 60, 60);
+
+    fill(0);
+    rect(395, 397, 50, 50);
+
+    fill(255);
+    rect(395, 397, 40, 40);
+
+    fill(0);
+    rect(395, 397, 30, 30);
+
+    fill(255);
+    rect(395, 397, 20, 20);
+
+    fill(0);
+    rect(395, 397, 10, 10);
+
+    //CUADRADOS UBICADOS EN EL CENTRO CON SUS RESPECTIVAS ROTACIONES
+    //cuadrado blanco del centro
+    push();
+    noStroke();
+    fill(0);
+    translate(width / 2, height / 2);
+    rotate(angulo);
+    rectMode(RADIUS);
+    rect(0, 0, 110, 110);
+    pop();
+
+    push();
+    noStroke();
+    fill(200, 70);
+    rectMode(RADIUS);
+    translate(width / 2, height / 2);
+    rotate(-angulo / 2);
+    rect(0, 0, 100, 100);
+    pop();
+
+    push();
+    noStroke();
+    fill(0);
+    rectMode(RADIUS);
+    translate(width / 2, height / 2);
+    rotate(angulo);
+    rect(0, 0, 90,90);
+    pop();
+
+    push();
+    noStroke();
+    fill(200);
+    rectMode(RADIUS);
+    translate(width / 2, height / 2);
+    rotate(-angulo / 2);
+    rectMode(RADIUS);
+    rect(0, 0, 85, 85);
+    pop();
+
+    push();
+    noStroke();
+    fill(0);
+    rectMode(RADIUS);
+    translate(width / 2, height / 2);
+    rotate(angulo);
+    rect(0, 0, 75, 75);
+    pop();
+
+    push();
+    noStroke();
+    fill(200,70);
+    rectMode(RADIUS);
+    translate(width / 2, height / 2);
+    rotate(-angulo / 2);
+    rect(0, 0, 65, 65);
+    pop();
+
+    push();
+    noStroke();
+    fill(0);
+    rectMode(RADIUS);
+    translate(width / 2, height / 2);
+    rotate(angulo);
+    rect(0, 0, 55, 55);
+    pop();
+
+    push();
+    noStroke();
+    fill(200);
+    rectMode(RADIUS);
+    translate(width / 2, height / 2);
+    rotate(-angulo);
+    rect(0, 0, 45, 45);
+    pop();
+
+    push();
+    noStroke();
+    fill(0);
+    rectMode(RADIUS);
+    translate(width / 2, height / 2);
+    rotate(angulo);
+    rect(0, 0, 35, 35);
+    pop();
+
+    push();
+    noStroke();
+    fill(200);
+    rectMode(RADIUS);
+    translate(width / 2, height / 2);
+    rotate(-angulo / 2);
+    rect(0, 0, 25, 25);
+    pop();
+
+    angulo = angulo + 0.01; //VELOCIDAD DE ROTACIÓN
+
+    //para que no se desplegue el menu del botón derecho
+    document.oncontextmenu = function () {
+      return false;
+    };
+
+
+    // mantener presionado el botón izquierdo o derecho para colorar el fondo del lienzo.
+    if (mouseIsPressed) {
+      if (mouseButton == LEFT) {
+        noStroke();
+        fill(random(256)); //elige un color de la lista random
+        rect(mouseX, mouseY, 10 + random(10), 10 + random(10));
+        //dibujar rectangulos en diferentes tamaños con diferentes matices de grises.
+      }
+    }
+
+    if (mouseIsPressed) {
+      if (mouseButton == RIGHT) {
+        noStroke();
+        fill(random(256)); //elige un color de la lista random
+        rect(mouseX, mouseY, 10 + random(60), 20 + random(60));
+        //dibujar rectangulos en tamaños más pequeños con diferentes matices de grises.
+      }
+    }
+  }
+}
---
+++ _projects/statement.md
@@ -0,0 +1,61 @@
+---
+layout: post
+title: "QR for me"
+date: 2022-06-28
+thumbnail: thumbnail.png
+dependencies:
+  - p5
+  - p5.sound
+---
+
+<div id="div-sketch">
+  <script type="text/javascript" src="sketch.js"></script>
+</div>
+
+_Carla María Barrera Aybar_, 2022.
+
+**Título de la obra**
+
+Esta producción artística está inspirada en obras de Lillian Schwartz en sus obras "Pixellation"(1970), pionera del arte electrónico. Predominan figuras primitivas como el
+cuadrado y el rectángulo en una escala de grises simulando un "código qr" que al hacer click, los cuadrados ubicados en el centro comienzan 
+a rotar en sentidos contrarios simulando una pequeña máquina. Como complemento presionando el click derecho o izquierdo podemos conocer más 
+información en el lienzo gracias a un random permitiéndonos diseñar nuestro propio qr dibujando con cuadrados y rectángulos de diferentes 
+tamaños de manera única en cada reproducción. 
+
+**Desarrollo técnico, estético y conceptual**
+
+Esta producción me llevo a entender que la mayoría de las veces existe una distancia entre la concepción inicial y el producto final. Empezó como
+ un dibujo abstracto de cuadrados estáticos que luego adquirieron movimiento y en conjunto terminaron generando en mí una “idea de Qr”. 
+Me acuerdo lo sorprendida que quedé cuando escaneé por primera vez uno. Pensé lo cerca que estamos las personas inmersas en un sistema capitalista de ser un 
+conjunto de barras y números que almacenan mucha información. Esto me llevo a preguntarme ¿por qué no hacer que les espectadores interactúen con la obra simulando
+el código Qr que les gustaría ser? con el que se sientan más identificadxs. Uno que vaya más allá del blanco y el negro, que tenga diferentes matices 
+y composiciones. 
+
+Nathalie Heinich en "la obra más allá del objeto" plantea que el arte contemporaneo consiste en proyectar la obras de arte más allá del objeto y
+esa disolucion entre obra-objeto se da por una serie de operaciones específicas propias del paradigma del arte contemporáneo de las cuales 
+dos me parecieron interesantes destacar y desarrollar:
+
+Por una lado, la operación de desmaterialización _"lo informe"_ donde la autora plantea que
+ >..."el objeto es tan solo un pretexto, como mucho un activador, que inducirá acciones , palabras, operaciones, reconfiguraciones del espacio
+ ...y la obra está en ese conjunto de cosas... lejos de reducirse a los límites materiales del objeto , puede verse enriquecida por los comentarios, interpretaciones, imitaciones e incluso por todos los actos de vandalismo que 
+pueda suscitar"...
+
+Si bien mi obra tiene una estructura algoritmica y esta plasma una idea, solo funciona y esta completa si el espectador interactua con ella. Su dinamismo y composición dependera de quien
+interactue y como lo haga y puede causar en él distintas reacciones que también hacen a la obra. 
+Por otro lado, la operación de conceptualización _"la idea"_. Heinich cita a Nadia Walravens 
+
+>..."la idea es la obra y se reinvindica como tal por los artistas"...
+
+ La idea casi poética de poder construir algo propio y  a la vez tan cotidiano para nosotrxs 
+como un código esta presente en la obra y  va acompañada de una breve descripción o una serie de instrucciones a seguir para que el espectador entienda cual es la intención. 
+
+Instrucciones para interactuar:
+1) presionar "Enter" para ver la estructura inicial
+2) presionar el botón de mouse "izquierdo" o "derecho" para dibujar 
+3) dibujar como desee 
+
+**Bibliografía**
+
+
+Bibliografía
+HEINICH, Nathalie. (2014). "La obra más allá del objeto"
