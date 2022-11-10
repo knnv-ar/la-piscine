3f12f942f2589a1ef58ebdd505209aaecc3ea053
MauGut <115647931+MauGut@users.noreply.github.com>
2022-11-10 01:39:46 -0300

    Update sketch.js

--- _projects/sad-ghost-experiment-upload/sketch.js
+++ _projects/sad-ghost-experiment-upload/sketch.js
@@ -30,7 +30,7 @@ function setup() {
     });
   
   let onOff=botonOnOff;
-  onOff.id('div-sketch');
+  onOff.parent('div-sketch');
   
   seleccion = createSelect().position(5, 50).style ("font-family","courier");
   seleccion.option("sine");
@@ -42,7 +42,7 @@ function setup() {
   });
   
   let sel=seleccion;
-  sel.id('div-sketch');
+  sel.parent('div-sketch');
   
   volumen = createSlider(0, 0.1, 0, 0).position(0, 80);
   volumen.input(function () {
@@ -50,7 +50,7 @@ function setup() {
   });
   
   let des=volumen;
-  des.id('div-sketch');
+  des.parent('div-sketch');
 }
 
 function draw() {
