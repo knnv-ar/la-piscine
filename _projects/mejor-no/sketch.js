f7bd09efdef3e9dccb823cbb382102ea0d59420f
Silvina <msilvinahippler@gmail.com>
2025-11-18 14:09:08 -0300

    Silvina Hippler TP2

---
+++ _projects/mejor-no/sketch.js
@@ -0,0 +1,24 @@
+let img1, img2;
+
+function preload(){
+ img1 = loadImage("imagen/piel.png");
+ img2 = loadImage("imagen/emoji.png");
+}
+function setup() {
+  createCanvas(400, 400, WEBGL);
+  
+}
+
+function draw() {
+  background(200);
+  
+  image(img1, -400, -400, 800, 800);
+  
+  //rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
+ //push();
+  noStroke();
+  texture(img2);
+  sphere(70);//Parámetros: radio
+  //pop();
+}
