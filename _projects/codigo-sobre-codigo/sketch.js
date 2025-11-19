13e6b8d378fcc4323a33f1235837a4ce62bbbb7a
MauGut <115647931+MauGut@users.noreply.github.com>
2025-11-19 14:22:36 -0300

    ok

--- _projects/codigo-sobre-codigo/sketch.js
+++ _projects/codigo-sobre-codigo/sketch.js
@@ -15,7 +15,7 @@ function preload() {
 function setup() {
   let canvas = createCanvas(600, 655);
   canvas.parent("div-scketch");
-  img.resize(width, height);
+  img.resize(600, 655);
   background(255);
   textAlign(CENTER, CENTER);
   noStroke();
